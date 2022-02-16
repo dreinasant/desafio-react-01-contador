bb0e6918b93151a9eb71d34f1e9dfa9daeade1e5
dreinasant <ao.andreina@gmail.com>
2022-02-16 19:40:19 -0300

    desafio-contador-01

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import  Hamburguer from "./assets/hamburguer.jpg";
+import  Laranja from "./assets/laranja.png";
+import Bolo from "./assets/bolo.jpg";
+import  Melancia from "./assets/melancia.jpg";
+import  Pera from "./assets/pera.png";
+import  Sorvete from "./assets/sorvete.jpg";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class contador extends Component {
 
-export default App;
+  state = {
+    numero: 0
+  };
+
+  add = () => {
+    if (this.state.numero < 10) {
+      this.setState({
+        numero: this.state.numero + 1
+      });
+    }
+  };
+  
+  remover = () => {
+    if(this.state.numero > 0){
+      this.setState({
+        numero: this.state.numero - 1
+      })
+    }
+  }
+  
+  clear = () => {
+    this.setState({
+      numero: 0
+    })
+  }
+  
+    render() {
+      return (
+        <div className="contain">
+          <h1>Pou Contador</h1>
+          <h2>{this.state.numero}</h2>
+          <img src={Hamburguer} class="box-ham" alt="hamburguer"/>
+          <img src={Laranja} class="box-orange" alt="laranja"/>
+          <img src={Bolo} class="box-cake" alt="bolo"/>
+          <img src={Melancia} class="box-watermelon" alt="melancia"/>
+          <img src={Pera} class="box-pear" alt="pera"/>
+          <img src={Sorvete} class="box-icecream" alt="sorvete"/>
+          <div className="box">
+          <div className="box-pou"></div>
+          <div className="box-eye"></div>
+          <div className="intro-eye"></div>
+          <div class="contain-intro"></div>
+          <button onClick={this.add}>+</button>
+          <button onClick={this.remover}>-</button>
+          </div>
+          <div className="intro-clear">
+          <button onClick={this.clear}>C</button>
+          </div>
+          </div>
+      );
+    }
+  }
+  
+  export default contador;
